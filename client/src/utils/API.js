78a71a3444202886e9151890c5ddbc61e823182f
SL <samanthl795@gmail.com>
2021-08-28 15:48:06 -0700

    Remove unused function

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,15 +1,3 @@
-// save book data for a logged in user
-export const saveBook = (bookData, token) => {
-  return fetch('/api/users', {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-      authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(bookData),
-  });
-};
-
 // make a search to google books api
 // https://www.googleapis.com/books/v1/volumes?q=harry+potter
 export const searchGoogleBooks = (query) => {
